cd25265d4bd5bda565543dffe29487ee03259f82
Mayank Singhal <maysingh9@publicisgroupe.net>
2024-02-19 17:53:57 +0530

    Fixed run-time error

--- presentation/src/main/java/com/mayank/presentation/MainActivity.kt
+++ presentation/src/main/java/com/mayank/presentation/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.navigation.compose.rememberNavController
 import com.mayank.presentation.navigation.NavGraph
 import com.mayank.presentation.ui.theme.RickMortyTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
